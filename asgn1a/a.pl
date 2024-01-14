4843da12c818667c37a4dbc55ec8da318b73fbf8
adityjha0 <aditya2005aps@gmail.com>
2024-01-14 19:49:44 +0530

    testing

--- asgn1a/a.pl
+++ asgn1a/a.pl
@@ -5,23 +5,14 @@ mem(X,[]) :- fail.
 mem(X,[X|_]) :- !.
 mem(X,[_|R]) :- mem(X,R), !.
 
-/*reflexive closure*/
-mem((X,X), refclos(R,S)) :- mem(X,S), !.
-mem((X,Y), refclos(R,S)) :- mem((X,Y), R), !.
-
-/*transitive closure*/
-mem((X,Y), tranclos(R)) :- mem((X,Y), R), !.
-mem((X,Z), tranclos(R)) :- mem((X,Y), R), mem((Y,Z), tranclos(R)), !.
-
-/*symmetric closure*/
-mem((X,Y), symclos(R)) :- mem((X,Y), R), !.
-mem((Y,X), symclos(R)) :- mem((X,Y), R), !.
-
 /*reflexive-transitive closure*/
-mem((X,Y), reftranclos(R,S)) :- mem((X,Y), refclos(R,S)), !.
-mem((X,Y), reftranclos(R,S)) :- mem((X,Y), tranclos(R)), !.
+mem((X,Y), reftranclos(R,S)) :- mem((X,Y), R), !.
+mem((X,X), reftranclos(R,S)) :- mem(X, S), !.
+mem((X,Y), reftranclos(R,S)) :- mem((X,Z), R), mem((Z,Y), reftranclos(R,S)), !.
 
 /*reflexive-transitive-symmetric closure*/
-mem((X,Y), reftransymclos(R,S)) :- mem((X,Y), refclos(R,S)), !.
-mem((X,Y), reftransymclos(R,S)) :- mem((X,Y), tranclos(R)), !.
-mem((X,Y), reftransymclos(R,S)) :- mem((X,Y), symclos(R)), !.
+mem((X,X), reftransymclos(R,S)) :- mem(X, S), !.
+mem((X,Y), reftransymclos(R,S)) :- mem((Y,X), R), !.
+mem((X,Y), reftransymclos(R,S)) :- mem((Y,X), reftransymclos(R,S)), !.
+mem((X,Y), reftransymclos(R,S)) :- mem((X,Z), R), mem((Z,Y), reftransymclos(R,S)), !.
+mem((X,Y), reftransymclos(R,S)) :- mem((X,Z), reftransymclos(R,S)), mem((Z,Y), reftransymclos(R,S)), !.
