a6744445557fff794bc461b7d53ace14fb040fa4
adityjha0 <aditya2005aps@gmail.com>
2024-01-16 17:54:08 +0530

    ~

--- asgn1a/program.pl
+++ asgn1a/program.pl
@@ -1,5 +1,13 @@
 /*----------------------A----------------------*/
+/*membership*/
+mem(X,[]) :- fail.
+mem(X,[X|_]) :- !.
+mem(X,[_|R]) :- mem(X,R), !.
 
+/*-------1-------*/
+
+
+/*-------2-------*/
 
 
 /*----------------------B----------------------*/
@@ -24,7 +32,7 @@ mapcons(X,[Y|R],[[X|Y]|Z]):- mapcons(X,R,Z).
 powerI([],[[]]):- !.
 powerI([X|R],P):- powerI(R,P1), mapcons(X,P1,P2), append(P2,P1,P).
 
-/*1*/
+/*-------1-------*/
 /*
 Examples for unionI:
 
@@ -43,7 +51,6 @@ Examples for unionI:
     Input : unionI([9,1,7,4,0],[0,1,9,4,7],U).
     Output : U = [9, 1, 7, 4, 0].
 
-All these U are indeed unions of the given sets.
 */
 
 /*
@@ -64,10 +71,9 @@ Examples for powerI:
     Input : powerI([[0],1,2],P).
     Output: P = [[[0], 1, 2], [[0], 1], [[0], 2], [[0]], [1, 2], [1], [2], []].
 
-All these P are indeed powersets of the given set. 
 */
 
-/*2*/
+/*-------2-------*/
 /*
 Examples to check that union doesn't have duplicates.
     
@@ -83,35 +89,29 @@ Examples to check that union doesn't have duplicates.
     Output : U = [a, 2, q, 10].
     a, 2 are not taken twice.
 
-These show that the union doesn't have duplicates.
 */
 
-/*membership*/
-mem(X,[]) :- fail.
-mem(X,[X|_]) :- !.
-mem(X,[_|R]) :- mem(X,R), !.
-
-/*3*/
+/*-------3-------*/
 /*Intersection*/
 interI(S1,[],[]) :- !.
 interI([],S2,[]) :- !.
 interI([X|L],S2,[X|Z]) :- mem(X,S2), interI(L,S2,Z), !.
 interI([_|L],S2,Z) :- interI(L,S2,Z).
 
-/*4*/
+/*-------4-------*/
 /*Set Difference*/
 diffI(S1,[],S1) :- !.
 diffI([],S2,[]) :- !.
 diffI([X|L],S2,[X|Z]) :- \+ mem(X,S2), diffI(L,S2,Z), !.
 diffI([_|L],S2,Z) :- diffI(L,S2,Z).
 
-/*5*/
+/*-------5-------*/
 /*Cartesian Product*/
 cartesianI([],S2,[]) :- !.
 cartesianI(S1,[],[]) :- !.
 cartesianI([X|L],[Y|R],[(X,Y)|Z]) :- cartesianI(L,[Y|R],Z1), cartesianI([X|L],R,Z2), unionI(Z1,Z2,Z).
 
-/*6*/
+/*-------6-------*/
 /*
 Examples to check if the intersection implementation is correct.
 
@@ -130,50 +130,53 @@ Examples to check if the intersection implementation is correct.
     Input : interI([1,3,5,2,4],[6,1,2],I).
     Output : I = [1, 2].
 
-Surely, this implementation finds the intersection of two sets without faults.
 */
 
 
 /*
 Examples to check if the set-difference implementation is correct.
 
-    Input : 
-    Output : 
+    Input : diffI([],[1,2,3,4],D).
+    Output : D = [].
 
-    Input : 
-    Output : 
+    Input : diffI([1,2,3,4],[],D).
+    Output : D = [1, 2, 3, 4].
 
-    Input : 
-    Output : 
+    Input : diffI([1,2,3,4],[1,3],D).
+    Output : D = [2, 4].
 
-    Input : 
-    Output : 
+    Input : diffI([1,2,3,4],[1,2,3,4],D).
+    Output : D = [].
 
-    Input : 
-    Output : 
-
+    Input : diffI([1,2,3,d,42],[1,2,3,4,d],D).
+    Output : D = [42].
 
 */
 
 /*
 Examples to check if the cartesian product implementation is correct.
 
-    Input : 
-    Output : 
+    Input : cartesianI([1,2,3,4],[],C).
+    Output : C = [].
 
-    Input : 
-    Output : 
+    Input : cartesianI([1,2,3,4],[5],C).
+    Output : C = [(1, 5),  (2, 5),  (3, 5),  (4, 5)].
 
-    Input : 
-    Output : 
+    Input : cartesianI([1],[2,3,4,5],C).
+    Output : C = [(1, 2),  (1, 3),  (1, 4),  (1, 5)].
 
-    Input : 
-    Output : 
+    Input : cartesianI([1,2,3,4],[1,2,3,4,5],C).
+    Output : C = [(1, 1),  (2, 1),  (3, 1),  (4, 1),  (4, 2),  (4, 3),  (4, 4),  (4, 5),  (3, 2),  (3, 3),  (3, 4),  (3, 5),  (2, 2),  (2, 3),  (2, 4),  (2, 5),  (1, 2),  (1, 3),  (1, 4),  (1, 5)] .
 
-    Input : 
-    Output : 
+    Input : cartesianI([1,2,3,4],[a,b,c],C).
+    Output : C = [(1, a),  (2, a),  (3, a),  (4, a),  (4, b),  (4, c),  (3, b),  (3, c),  (2, b),  (2, c),  (1, b),  (1, c)].
+
+*/
+
+/*-------7-------*/
+arepowersetsequal(P1,P2).
+/*
 
 
 */
 
-/*7*/
