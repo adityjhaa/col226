f48a17d438461b91b06bf9029274e2af537ab739
adityjha0 <aditya2005aps@gmail.com>
2024-01-28 16:30:11 +0530

    the compiler

--- asgn2/toy.pl
+++ asgn2/toy.pl
@@ -8,7 +8,7 @@ calculate(plus(E1,E2), numeral(N)) :- calculate(E1, numeral(N1)), calculate(E2, 
 calculate(times(E1,E2), numeral(N)) :- calculate(E1, numeral(N1)), calculate(E2, numeral(N2)), N is N1*N2.
 
 /*
-A test case:- 
+A test case:
 calculate(plus(times(numeral(3), numeral(4)), times(numeral(5), numeral(6))), numeral(N)).
 
 N = 42.
@@ -16,5 +16,35 @@ N = 42.
 
 append([], L, L).
 append([X|R], L, [X|Z]):- append(R, L, Z).
+
+/*
+Compiler
+*/
+
+compile(numeral(N),[ldop(N)]) :- integer(N).
+compile(plus(E1,E2),C ) :- compile(E1, C1), compile(E2, C2), append(C1, C2, C3), append(C3, [plusop], C). 
+compile(times(E1,E2),C ) :- compile(E1, C1), compile(E2, C2), append(C1, C2, C3), append(C3, [timesop], C). 
+
+
+/*
+The Stack Machine
+*/
+
+stackmc( [A|S1], [ ], A).
+stackmc( S, [ldop(N)|C], A) :- stackmc([N|S], C, A).
+stackmc( [N2|[N1|S]], [plusop|C], A ) :- N is N1+N2, stackmc([N|S], C, A).
+stackmc( [N2|[N1|S]], [timesop|C], A ) :- N is N1*N2, stackmc([N|S], C, A).
+
+
+/*
+Suggested test query:
+compile(plus( times(numeral(3), numeral(4)), times(numeral(5), numeral(6)) ), C),
+stackmc( [ ], C, A).
+
+C = [ldop(3), ldop(4), timesop, ldop(5), ldop(6), timesop, plusop],
+A = 42 .
+
+*/
+
 
 
