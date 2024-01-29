f27eb2f7ec30cd91fecbf97756a3b35e87887038
adityjha0 <aditya2005aps@gmail.com>
2024-01-30 02:43:59 +0530

    explainations

--- asgn2/program.pl
+++ asgn2/program.pl
@@ -4,19 +4,23 @@ mem(X,[X|_]) :- !.
 mem(X,[_|R]) :- mem(X,R), !.
 
 
+/*base cases*/
 hastype(G,intT(N),intT):- !.
 hastype(G,boolT(B),boolT):- !.
 hastype(G,varT(X),intT):- mem((X,intT),G).
 hastype(G,varT(X),boolT):- mem((X,boolT),G).
+/*int arguments with int result*/
 hastype(G,add(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,subt(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,mul(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,divt(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,max(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,min(E1,E2),intT):- hastype(G,E1,intT), hastype(G,E2,intT).
+/*int arguments with bool result*/
 hastype(G,gtn(E1,E2),boolT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,ltn(E1,E2),boolT):- hastype(G,E1,intT), hastype(G,E2,intT).
 hastype(G,eql(E1,E2),boolT):- hastype(G,E1,intT), hastype(G,E2,intT).
+/*bool arguments with bool result*/
 hastype(G,myand(E1,E2),boolT):- hastype(G,E1,boolT), hastype(G,E2,boolT).
 hastype(G,myor(E1,E2),boolT):- hastype(G,E1,boolT), hastype(G,E2,boolT).
 hastype(G,mynot(E1),boolT):- hastype(G,E1,boolT).
