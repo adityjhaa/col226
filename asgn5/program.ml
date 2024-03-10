c639b230b561ae7ddf1377755728f435b316901f
adityjha0 <aditya2005aps@gmail.com>
2024-03-10 12:36:14 +0530

    clean

--- asgn5/program.ml
+++ asgn5/program.ml
@@ -9,13 +9,15 @@ let check_sig (sign : signature ) : bool =
   let rec check_dup l =
     match l with
     | [] -> false
-    | a::t -> if (List.mem a t) then true
+    | a::t -> 
+      if (List.mem a t) then true
       else check_dup t
   in
   let rec check_neg l =
     match l with
     | [] -> false
-    | a::t -> if a<0 then true
+    | a::t -> 
+      if a<0 then true
       else check_neg t
   in 
   not (check_dup (List.map fst sign)) && not (check_neg (List.map snd sign)) 
@@ -40,20 +42,23 @@ let rec wftree (t: tree) (sign: signature) : bool =
 let rec ht (t: tree) : int =
   match t with
   | V _ -> 0
-  | C {node = n ; children = clist} -> 1 + List.fold_left (fun acc tx -> max acc (ht tx)) 0 clist
+  | C {node = n ; children = clist} -> 
+    1 + List.fold_left (fun acc tx -> max acc (ht tx)) 0 clist
 ;;
 
 let rec size (t: tree) :int =
   match t with
   | V _ -> 1
-  | C {node = n ; children = clist} -> 1 + List.fold_left (fun acc tx -> acc + (size tx)) 0 clist 
+  | C {node = n ; children = clist} -> 
+    1 + List.fold_left (fun acc tx -> acc + (size tx)) 0 clist 
 ;;
 
 let vars (t: tree) : string list =
   let rec vars_helper ty acc =
     match ty with
     | V x -> x::acc
-    | C {node = n ; children = clist} -> List.fold_left(fun acc tx -> vars_helper tx acc) acc clist
+    | C {node = n ; children = clist} -> 
+      List.fold_left(fun acc tx -> vars_helper tx acc) acc clist
   in
     List.sort_uniq compare (vars_helper t [])
 ;;
@@ -61,7 +66,8 @@ let vars (t: tree) : string list =
 let rec mirror (t:tree) :tree =
   match t with
   | V x -> V x
-  | C {node = n ; children = clist} -> C {node = n ; children = List.rev (List.map mirror clist)}
+  | C {node = n ; children = clist} -> 
+    C {node = n ; children = List.rev (List.map mirror clist)}
 ;;
 
 let rec check_subst (subst: substitution) : bool =
