58c5c517285eee7d8a2953d62b733a3a0de3c429
adityjha0 <aditya2005aps@gmail.com>
2024-03-12 17:57:41 +0530

    mgu

--- asgn5/program.ml
+++ asgn5/program.ml
@@ -1,3 +1,5 @@
+exception NOT_UNIFIABLE;;
+
 type symbol = (string * int);;
 type signature = symbol list;;
 
@@ -96,18 +98,50 @@ let subst (sub : substitution) (t: tree) : tree =
 
 (* The basic implementation *)
 
-(* let compose_subst (s1:substitution) (s2:substitution) (t:tree) : tree =
+(* 
+let compose_subst (s1:substitution) (s2:substitution) (t:tree) : tree =
   subst s2 (subst s1 t)
-;; *)
+;; 
+*)
 
-let compose_subst (s1: substitution) (s2: substitution) (t: tree) : tree =
-  let rec helper t =
-    match t with
-    | V x -> 
-      (try List.assoc x s1 with Not_found -> 
-        (try List.assoc x s2 with Not_found -> V x))
-    | C {node; children} -> 
-      C {node; children = List.map helper children}
+let rec compose_subst (s1: substitution) (s2: substitution) : substitution =
+  let rec compose_helper acc = function
+  | [] -> acc
+  | (var, t) :: rest ->
+    let t' = subst_tree t s1 in
+    let acc' = if List.mem_assoc var s1 then acc else (var, t') :: acc in
+    compose_helper acc' rest
   in
-  helper t
+  compose_helper s1 s2
+;;
+
+let rec occur_check (x: string) (t: tree) : bool = 
+  match t with
+  | V a -> a=x
+  | C {node = _ ; children = clist} ->
+    List.exists (occur_check x) clist
+;;
+ 
+let rec mgu (t:tree) (u:tree) : substitution =
+  match t, u with
+  | V x, V y ->
+    if x=y then [(x, V x)]
+    else [(x, V y)]
+  | V x, C r ->
+    if occur_check x u then raise NOT_UNIFIABLE
+    else [(x, u)]
+  | C r, V x ->
+    if occur_check x t then raise NOT_UNIFIABLE
+    else [(x, t)]
+  | C {node = n1; children = c1}, C {node = n2; children = c2} when n1 = n2 ->
+    let rec helper sub c1 c2 =
+      match c1, c2 with
+      | [], [] -> sub
+      | c1h::c1t, c2h::c2t ->
+        let s = compose_subst sub (mgu (subst sub c1h) (subst sub c2h)) in
+        helper s c1t c2t
+      | _, _ -> raise NOT_UNIFIABLE
+    in 
+    helper [] c1 c2
+  | _, _ -> raise NOT_UNIFIABLE
 ;;
