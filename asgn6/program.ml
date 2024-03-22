d58cc1186f61de8de43d6cecfbd3e1f552b535c7
adityjha0 <aditya2005aps@gmail.com>
2024-03-22 19:19:03 +0530

    proper types

--- asgn6/program.ml
+++ asgn6/program.ml
@@ -1,15 +1,13 @@
-type exp = Num of int | Bl of bool | V of string | Plus of exp * exp | Minus of exp * exp | Times of exp * exp | And of exp * exp | Or of exp * exp | Not of exp | Gt of exp * exp | Lt of exp * exp | Eq of exp * exp | IfTE of exp * exp * exp | Pair of exp * exp | Fst of exp * exp | Snd of exp * exp ;;
+type variable = V of string;;
 
-type values = N of int | B of bool | P of values * values;;
+type exp = Num of int | Bl of bool | V of string | Plus of exp * exp | Minus of exp * exp | Times of exp * exp | And of exp * exp | Or of exp * exp | Not of exp | Gt of exp * exp | Lt of exp * exp | Eq of exp * exp | IfTE of exp * exp * exp | Pair of exp * exp | Fst of exp * exp | Snd of exp * exp | Abs of string * exp | App of exp * exp;;
 
-type opcode = LDN of int | LDB of bool | LOOKUP of string | | PLUS | TIMES | MINUS | AND | OR | NOT | GT | LT | EQ | COND of opcode list * opcode list | PAIR | FST | SND;;
+type opcode = LDN of int | LDB of bool | LOOKUP of string | PLUS | TIMES | MINUS | AND | OR | NOT | GT | LT | EQ | COND of opcode list * opcode list | PAIR | FST | SND | APP | MKCLOS of string * (opcode list) | RET;;
 
-type stack = values list;;
-
-type environment = (var of string * values) list;;
-
-type code = opcode list;;
-
-type dump = (stack * environment * code) list;;
+type values = N of int | B of bool | P of values * values | Clos of variable * exp * environment
+and stack = values list
+and environment = (variable * values) list
+and code = opcode list
+and dump = (stack * environment * code) list;;
 
 
